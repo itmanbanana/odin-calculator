ef870a261872bebf9742959284f4d7b9816660b8
itmanbanana <itmanbanana@gmail.com>
2026-01-28 20:28:04 -0500

    Add basic styles

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const add = (a, b) => { return a + b; }
 const subtract = (a, b) => { return a - b; }
 const multiply = (a, b) => { return a * b; }
-const divide = (a, b) => { return a / b; }
+const divide = (a, b) => { return a / b; } 
 
 function operate(a, b, op) {
     switch(op) {
